fa020b2e51cf629134ff9b52ca81b1d9d956107f
zielapio <piotr.zielinski.cv@gmail.com>
2020-06-22 13:57:30 +0200

    writing to one file and more flattened code

--- tlsp_scores_csv.py
+++ tlsp_scores_csv.py
@@ -6,30 +6,33 @@ seasons = ['2020', '2019', '2018', '2017']
 levels = ['ek', '1l', '2l', '3l', '4l', 'ol']
 
 
-def pitch_scores():
-    for season in seasons:
-        for level in levels:
-            www = f"http://tlsp.pl/wyniki/{season}/lnb/b{level}/"
-            get_www = requests.get(www)
-            source_www = get_www.content
-            soup_www = BeautifulSoup(source_www, 'lxml')
-            try:
-                soup_www.find("div", class_="wiersz").contents[0]
-                pass
-            except:
-                file_name = f"pitch_{season}_{level}"
-                with open(f"{file_name}.csv", "w", newline='') as file_with_results:
-                    the_writer = csv.writer(file_with_results)
-                    for result in soup_www.find_all("tr"):
-                        if result.find("td", class_="wyn_wynik") is not None:
-                            team_1 = result.find("td", class_="wyn_druzynad").find("a").contents[0]
-                            team_2 = result.find("td", class_="wyn_druzynaw").find("a").contents[0]
-                            try:
-                                score_team_1 = result.find("td", class_="wyn_wynik").find("b").contents[0].split(':')[0].strip()
-                                score_team_2 = result.find("td", class_="wyn_wynik").find("b").contents[0].split(':')[1].strip()
-                            except:
-                                pass
-                            the_writer.writerow([team_1, team_2, score_team_1, score_team_2])
+def tlsp_scores(place):
+    www_address_part_1 = 'lnb'
+    www_address_part_2 = 'b'
+    if place == 'hall':
+        www_address_part_1 = 'hlp'
+        www_address_part_2 = 'h'
+    file_name = f"{place}_scores"
+    with open(f"{file_name}.csv", "w", newline='') as file_with_results:
+        the_writer = csv.writer(file_with_results)
+        for season in seasons:
+            for level in levels:
+                www = f"http://tlsp.pl/wyniki/{season}/{www_address_part_1}/{www_address_part_2}{level}/"
+                get_www = requests.get(www)
+                source_www = get_www.content
+                soup_www = BeautifulSoup(source_www, 'lxml')
+                if soup_www.find("div", class_="wiersz") is not None:
+                    continue
+                the_writer.writerow([f"{season}_{level}"])
+                for result in soup_www.find_all("tr"):
+                    if result.find("td", class_="wyn_wynik") is None:
+                        continue
+                    team_1, team_2 = result.find("td", class_="wyn_druzynad").find("a").contents[0], result.find("td", class_="wyn_druzynaw").find("a").contents[0]
+                    if result.find("td", class_="wyn_wynik").find("b") is None:
+                        continue
+                    score_team_1, score_team_2 = result.find("td", class_="wyn_wynik").find("b").contents[0].split(':')[0].strip(), result.find("td", class_="wyn_wynik").find("b").contents[0].split(':')[1].strip()
+                    the_writer.writerow([team_1, team_2, score_team_1, score_team_2])
+
 
 
 
--- tlsp_scraping.py
+++ tlsp_scraping.py
@@ -1,12 +1,9 @@
-from TLSP_scraping.tlsp_scores_csv import pitch_scores
-from TLSP_scraping.tlh_scores_csv import hall_scores
+from TLSP_scraping.tlsp_scores_csv import tlsp_scores
 from TLSP_scraping.players_info import players
 
 # import to csv files TLSP (tlsp.pl) results from season 2017 to season 2020
-# pitch_scores()
-
-# import to csv files TLH (tlsp.pl) results from season 2017/2018 to season 2019/2020
-# hall_scores()
+# tlsp_scores default value is pitch, optional is hall.
+tlsp_scores('pitch')
 
 # import to csv files info about all TLSP (tlsp.pl) players from season 2017 to season 2020
-players()
+#players()
