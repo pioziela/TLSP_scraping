4d070be92a4cb2258af9f3f1a624f00828101a20
zielapio <piotr.zielinski.cv@gmail.com>
2020-06-24 11:00:20 +0200

    downloading players data

--- players_info.py
+++ players_info.py
@@ -4,35 +4,44 @@ import csv
 
 
 def players():
-    player_id = 1
-    player_exist = True
-    # usunac player_id < 20
-    while player_exist is True and player_id < 20:
-        www = f"http://tlsp.pl/zawodnik/{player_id}/"
-        get_www = requests.get(www)
-        source_www = get_www.content
-        soup_www = BeautifulSoup(source_www, 'lxml')
-        player_name = soup_www.find("tr").find("b").contents[0]
-        file_name = f"player_{player_id}_{player_name.strip()}"
-        with open(f"TLSP_players/{file_name}.csv", "w", newline='') as file_with_player_info:
-                    the_writer = csv.writer(file_with_player_info)
-                    for player in soup_www.find_all("tr"):
-                        if player.find("td", class_="zaw_zawody") is not None and player.find("td", class_="zaw_zawody").find("a") is not None:
-                            for info in player:
-                                print(info.a)
+    file_name = f"players_TLSP_v2"
+    with open(f"{file_name}.csv", "w", newline='') as file_with_player_info:
+        the_writer = csv.writer(file_with_player_info)
+        player_id = 1
+        while player_id < 2500:
+            www = f"http://tlsp.pl/zawodnik/{player_id}/"
+            get_www = requests.get(www)
+            source_www = get_www.content
+            soup_www = BeautifulSoup(source_www, 'lxml')
+            if soup_www.find("div", class_="wiersz") is not None:
+                player_id += 1
+                continue
+            player_name = soup_www.find("tr").find("b").contents[0]
+            the_writer.writerow([player_name])
+            for tr in soup_www.find_all("tr"):
+                if tr.find("td", class_="zaw_pozycja") is None:
+                    continue
+                if tr.find("td", class_="zaw_pozycja").contents[0] == 'Pozycja':
+                    continue
+                if len(tr.find("td", class_="zaw_pozycja").contents[0]) < 2:
+                    continue
+                player_season_info = []
+                for td in tr.find_all("td"):
+                    player_season_info.append(td)
+                player_season = player_season_info[0].find("a").contents[0]
+                if len(player_season_info[1].find("a").contents) == 0:
+                    player_team = 'unknown'
+                else:
+                    player_team = player_season_info[1].find("a").contents[0]
+                player_position = player_season_info[2].contents[0]
+                player_matches = player_season_info[3].contents[0]
+                player_goals = player_season_info[4].contents[0]
+                player_yellow_cards = player_season_info[5].contents[0]
+                player_red_cards = player_season_info[6].contents[0]
+                the_writer.writerow([player_season, player_team, player_position, player_matches,
+                                    player_goals, player_yellow_cards, player_red_cards])
+            player_id += 1
+            print(player_id)
 
 
-                            """
-                            season = player.find("td", class_="zaw_zawody").find("a").contents[0]
-                            team = player.find("td", class_="zaw_druzyna").find("a").contents[0]
-                            position = player.find("td", class_="zaw_pozycja").contents[0]
-                            metches = player.find("td").contents[0]
-                            goals = player.find("td").contents[0]
-                            yellow_cards = player.find("td").contents[0]
-                            red_cards = player.find("td").contents[0]
-                            """
-                            #the_writer.writerow([season, team, position, metches, goals, yellow_cards, red_cards])
-
-
-
-
+players()
