96484b93ea80eecee7612ae8454ec43448284e67
zielapio <piotr.zielinski.cv@gmail.com>
2020-06-26 13:24:05 +0200

    slightly clearer code with more functions

--- tlsp_scores_statistcs.py
+++ tlsp_scores_statistcs.py
@@ -30,67 +30,16 @@ def team_statistics(data):
                                                 'The longest series without win'])
     for team in teams:
         all_team_matches = data.loc[(data['Team 1'] == team) | (data['Team 2'] == team)]
-        matches = len(all_team_matches)
-        win1, win2 = all_team_matches.loc[all_team_matches['Team 1 goals'] > all_team_matches['Team 2 goals']].loc[all_team_matches['Team 1'] == team], \
-                     all_team_matches.loc[all_team_matches['Team 2 goals'] > all_team_matches['Team 1 goals']].loc[all_team_matches['Team 2'] == team]
-        wins = len(pd.concat([win1, win2]))
-        win_percentage = (wins / matches) * 100
-        lost1, lost2 = all_team_matches.loc[all_team_matches['Team 1 goals'] < all_team_matches['Team 2 goals']].loc[all_team_matches['Team 1'] == team], \
-                     all_team_matches.loc[all_team_matches['Team 2 goals'] < all_team_matches['Team 1 goals']].loc[all_team_matches['Team 2'] == team]
-        losts = len(pd.concat([lost1, lost2]))
-        lost_percentage = (losts / matches) * 100
-        draws = len(all_team_matches.loc[all_team_matches['Team 1 goals'] == all_team_matches['Team 2 goals']])
-        draw_percentage = (draws / matches) * 100
+        matches = number_one_team_matches(all_team_matches)
+        wins = one_team_wins(all_team_matches, team)
+        losts = one_team_losts(all_team_matches, team)
+        draws = one_team_draws(all_team_matches)
+        win_percentage, lost_percentage, draw_percentage = matches_percentage(matches, wins, losts, draws)
         points = wins * 3 + draws
-        points_per_match = points / matches
-        goals_for = all_team_matches.loc[all_team_matches['Team 1'] == team, 'Team 1 goals'].sum() + \
-                    all_team_matches.loc[all_team_matches['Team 2'] == team, 'Team 2 goals'].sum()
-        goals_for_per_match = goals_for / matches
-        goals_against = all_team_matches.loc[all_team_matches['Team 1'] == team, 'Team 2 goals'].sum() + \
-                    all_team_matches.loc[all_team_matches['Team 2'] == team, 'Team 1 goals'].sum()
-        goals_against_per_match = goals_against / matches
+        goals_for, goals_against = goals_for_against(all_team_matches, team)
         goals_difference = goals_for - goals_against
-        win_series = 0
-        the_longest_win_series = 0
-        lost_series = 0
-        the_longest_lost_series = 0
-        series_without_lost = 0
-        the_longest_series_without_lost = 0
-        series_without_win = 0
-        the_longest_series_without_win = 0
-        for index, row in all_team_matches.iterrows():
-            if (row['Team 1'] == team and row['Team 1 goals'] > row['Team 2 goals']) or (row['Team 2'] == team and row['Team 2 goals'] > row['Team 1 goals']):
-                win_series += 1
-            else:
-                if win_series > the_longest_win_series:
-                    the_longest_win_series = win_series
-                win_series = 0
-            if (row['Team 1'] == team and row['Team 1 goals'] >= row['Team 2 goals']) or (row['Team 2'] == team and row['Team 2 goals'] >= row['Team 1 goals']):
-                series_without_lost += 1
-            else:
-                if series_without_lost > the_longest_series_without_lost:
-                    the_longest_series_without_lost = series_without_lost
-                series_without_lost = 0
-            if (row['Team 1'] == team and row['Team 1 goals'] < row['Team 2 goals']) or (row['Team 2'] == team and row['Team 2 goals'] < row['Team 1 goals']):
-                lost_series += 1
-            else:
-                if lost_series > the_longest_lost_series:
-                    the_longest_lost_series = lost_series
-                lost_series = 0
-            if (row['Team 1'] == team and row['Team 1 goals'] <= row['Team 2 goals']) or (row['Team 2'] == team and row['Team 2 goals'] <= row['Team 1 goals']):
-                series_without_win += 1
-            else:
-                if series_without_win > the_longest_series_without_win:
-                    the_longest_series_without_win = series_without_win
-                series_without_win = 0
-        if win_series > the_longest_win_series:
-            the_longest_win_series = win_series
-        if lost_series > the_longest_lost_series:
-            the_longest_lost_series = lost_series
-        if series_without_lost > the_longest_series_without_lost:
-            the_longest_series_without_lost = series_without_lost
-        if series_without_win > the_longest_series_without_win:
-            the_longest_series_without_win = series_without_win
+        points_per_match, goals_for_per_match, goals_against_per_match = per_match(matches, points, goals_for, goals_against)
+        the_longest_win_series, the_longest_lost_series, the_longest_series_without_lost, the_longest_series_without_win = the_longest_series(all_team_matches, team)
         all_team_statistics_2 = pd.DataFrame([[team, matches, points, round(points_per_match,2), wins, draws, losts,
                                             round(win_percentage,0), round(draw_percentage,0), round(lost_percentage,0),
                                              goals_for, goals_against, goals_difference, round(goals_for_per_match,2),
@@ -107,7 +56,96 @@ def team_statistics(data):
     return all_team_statistics
 
 
-#print(team_statistics(data_with_correct_team_names))
+def number_one_team_matches(team_matches):
+    matches = len(team_matches)
+    return matches
+
+
+def one_team_wins(team_matches, club):
+    win1 = team_matches.loc[team_matches['Team 1 goals'] > team_matches['Team 2 goals']].loc[team_matches['Team 1'] == club]
+    win2 = team_matches.loc[team_matches['Team 2 goals'] > team_matches['Team 1 goals']].loc[team_matches['Team 2'] == club]
+    wins = len(pd.concat([win1, win2]))
+    return wins
+
+
+def one_team_losts(team_matches, club):
+    lost1 = team_matches.loc[team_matches['Team 1 goals'] < team_matches['Team 2 goals']].loc[team_matches['Team 1'] == club]
+    lost2 = team_matches.loc[team_matches['Team 2 goals'] < team_matches['Team 1 goals']].loc[team_matches['Team 2'] == club]
+    losts = len(pd.concat([lost1, lost2]))
+    return losts
+
+
+def one_team_draws(team_matches):
+    draws = len(team_matches.loc[team_matches['Team 1 goals'] == team_matches['Team 2 goals']])
+    return draws
+
+
+def matches_percentage(matches, wins, losts, draws):
+    wins_per = wins / matches * 100
+    losts_per = losts / matches * 100
+    draws_per = draws / matches * 100
+    return wins_per, losts_per, draws_per
+
+
+def goals_for_against(team_matches, club):
+    goals_for = team_matches.loc[team_matches['Team 1'] == club, 'Team 1 goals'].sum() + \
+                team_matches.loc[team_matches['Team 2'] == club, 'Team 2 goals'].sum()
+    goals_against = team_matches.loc[team_matches['Team 1'] == club, 'Team 2 goals'].sum() + \
+                team_matches.loc[team_matches['Team 2'] == club, 'Team 1 goals'].sum()
+    return goals_for, goals_against
+
+
+def per_match(matches, points, goals_for, goals_against):
+    points_per_match = points / matches
+    goals_for_per_match = goals_for / matches
+    goals_against_per_match = goals_against / matches
+    return points_per_match, goals_for_per_match, goals_against_per_match
+
+
+def the_longest_series(team_matches, club):
+    win_series, the_longest_win_series, lost_series, the_longest_lost_series = 0, 0, 0, 0
+    series_without_lost, the_longest_series_without_lost, series_without_win, the_longest_series_without_win = 0, 0, 0, 0
+    for index, row in team_matches.iterrows():
+        if (row['Team 1'] == club and row['Team 1 goals'] > row['Team 2 goals']) or (
+                row['Team 2'] == club and row['Team 2 goals'] > row['Team 1 goals']):
+            win_series += 1
+        else:
+            if win_series > the_longest_win_series:
+                the_longest_win_series = win_series
+            win_series = 0
+        if (row['Team 1'] == club and row['Team 1 goals'] >= row['Team 2 goals']) or (
+                row['Team 2'] == club and row['Team 2 goals'] >= row['Team 1 goals']):
+            series_without_lost += 1
+        else:
+            if series_without_lost > the_longest_series_without_lost:
+                the_longest_series_without_lost = series_without_lost
+            series_without_lost = 0
+        if (row['Team 1'] == club and row['Team 1 goals'] < row['Team 2 goals']) or (
+                row['Team 2'] == club and row['Team 2 goals'] < row['Team 1 goals']):
+            lost_series += 1
+        else:
+            if lost_series > the_longest_lost_series:
+                the_longest_lost_series = lost_series
+            lost_series = 0
+        if (row['Team 1'] == club and row['Team 1 goals'] <= row['Team 2 goals']) or (
+                row['Team 2'] == club and row['Team 2 goals'] <= row['Team 1 goals']):
+            series_without_win += 1
+        else:
+            if series_without_win > the_longest_series_without_win:
+                the_longest_series_without_win = series_without_win
+            series_without_win = 0
+    if win_series > the_longest_win_series:
+        the_longest_win_series = win_series
+    if lost_series > the_longest_lost_series:
+        the_longest_lost_series = lost_series
+    if series_without_lost > the_longest_series_without_lost:
+        the_longest_series_without_lost = series_without_lost
+    if series_without_win > the_longest_series_without_win:
+        the_longest_series_without_win = series_without_win
+    return the_longest_win_series, the_longest_lost_series, the_longest_series_without_lost, the_longest_series_without_win
+
+
+print(team_statistics(data_with_correct_team_names))
 
 team_statistics(data_with_correct_team_names).to_csv(f"{pitch_or_hall}_tlsp_statistics.csv")
 
@@ -137,7 +175,7 @@ def general_statistics(data):
     return sorted_most_often_played, sorted_most_common_result
 
 
-often_played, common_result = general_statistics(data_with_correct_team_names)
+#often_played, common_result = general_statistics(data_with_correct_team_names)
 
-print(often_played)
-print(common_result)
+#print(often_played)
+#print(common_result)
